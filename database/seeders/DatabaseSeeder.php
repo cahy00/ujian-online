8c67f5717c393c1169d851b321b8bdd3a500cc9a
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-06 15:58:54 +0900

    #

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Classroom;
+use App\Models\Lesson;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+				Lesson::create([
+					'title' => 'Bahasa Indonesia'
+				]);
+
+				Classroom::create([
+					'title' => 'XII IPS 1'
+				]);
     }
 }
