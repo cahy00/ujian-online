c8ea6f5729e7f1c47cd800ffba48e34872cffc63
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-06 23:00:04 +0900

    #

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Classroom extends Model
+class Exam extends Model
 {
     use HasFactory;
 		protected $fillable = [
-			'title'
+			'title', 
+			'lesson_id',
+			'classroom_id',
+			'duration',
+			'description',
+			'random_question',
+			'random_answer',
+			'show_answer'
 		];
 }
