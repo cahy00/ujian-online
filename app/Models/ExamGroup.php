fd1d5aa38704af7f4b350c6c9a078f8440060b0c
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-07 09:11:37 +0900

    #

---
+++ app/Models/ExamGroup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExamGroup extends Model
+{
+    use HasFactory;
+		protected $fillable = [
+			'exam_id',
+			'exam_session_id',
+			'student_id'
+		];
+}
