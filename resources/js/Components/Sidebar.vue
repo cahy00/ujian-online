47d1083a2aa7adb33b060f7fdee8c1f8fd7fdd89
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-08 11:26:04 +0900

    #

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -4,44 +4,19 @@
             <div class="sb-sidenav-menu">
                 <div class="nav">
                     <div class="sb-sidenav-menu-heading">Core</div>
-                    <a class="nav-link" href="index.html">
+                    <Link
+                        href="/admin/dashboard"
+                        :class="{
+                            active: $page.url.startsWith('/admin/dashboard'),
+                        }"
+                        class="nav-link"
+                    >
                         <div class="sb-nav-link-icon">
                             <i class="fas fa-tachometer-alt"></i>
                         </div>
                         Dashboard
-                    </a>
-                    <div class="sb-sidenav-menu-heading">Interface</div>
-                    <a
-                        class="nav-link collapsed"
-                        href="#"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapseLayouts"
-                        aria-expanded="false"
-                        aria-controls="collapseLayouts"
-                    >
-                        <div class="sb-nav-link-icon">
-                            <i class="fas fa-columns"></i>
-                        </div>
-                        Layouts
-                        <div class="sb-sidenav-collapse-arrow">
-                            <i class="fas fa-angle-down"></i>
-                        </div>
-                    </a>
-                    <div
-                        class="collapse"
-                        id="collapseLayouts"
-                        aria-labelledby="headingOne"
-                        data-bs-parent="#sidenavAccordion"
-                    >
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="layout-static.html"
-                                >Static Navigation</a
-                            >
-                            <a class="nav-link" href="layout-sidenav-light.html"
-                                >Light Sidenav</a
-                            >
-                        </nav>
-                    </div>
+                    </Link>
+                    <div class="sb-sidenav-menu-heading">Modul</div>
                     <a
                         class="nav-link collapsed"
                         href="#"
@@ -53,97 +28,46 @@
                         <div class="sb-nav-link-icon">
                             <i class="fas fa-book-open"></i>
                         </div>
-                        Pages
-                        <div class="sb-sidenav-collapse-arrow">
-                            <i class="fas fa-angle-down"></i>
-                        </div>
+                        Mata Pelajaran
                     </a>
-                    <div
-                        class="collapse"
-                        id="collapsePages"
-                        aria-labelledby="headingTwo"
-                        data-bs-parent="#sidenavAccordion"
+                    <a
+                        class="nav-link collapsed"
+                        href="#"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseLayouts"
+                        aria-expanded="false"
+                        aria-controls="collapseLayouts"
                     >
-                        <nav
-                            class="sb-sidenav-menu-nested nav accordion"
-                            id="sidenavAccordionPages"
-                        >
-                            <a
-                                class="nav-link collapsed"
-                                href="#"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#pagesCollapseAuth"
-                                aria-expanded="false"
-                                aria-controls="pagesCollapseAuth"
-                            >
-                                Authentication
-                                <div class="sb-sidenav-collapse-arrow">
-                                    <i class="fas fa-angle-down"></i>
-                                </div>
-                            </a>
-                            <div
-                                class="collapse"
-                                id="pagesCollapseAuth"
-                                aria-labelledby="headingOne"
-                                data-bs-parent="#sidenavAccordionPages"
-                            >
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="login.html"
-                                        >Login</a
-                                    >
-                                    <a class="nav-link" href="register.html"
-                                        >Register</a
-                                    >
-                                    <a class="nav-link" href="password.html"
-                                        >Forgot Password</a
-                                    >
-                                </nav>
-                            </div>
-                            <a
-                                class="nav-link collapsed"
-                                href="#"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#pagesCollapseError"
-                                aria-expanded="false"
-                                aria-controls="pagesCollapseError"
-                            >
-                                Error
-                                <div class="sb-sidenav-collapse-arrow">
-                                    <i class="fas fa-angle-down"></i>
-                                </div>
-                            </a>
-                            <div
-                                class="collapse"
-                                id="pagesCollapseError"
-                                aria-labelledby="headingOne"
-                                data-bs-parent="#sidenavAccordionPages"
-                            >
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="401.html"
-                                        >401 Page</a
-                                    >
-                                    <a class="nav-link" href="404.html"
-                                        >404 Page</a
-                                    >
-                                    <a class="nav-link" href="500.html"
-                                        >500 Page</a
-                                    >
-                                </nav>
-                            </div>
-                        </nav>
-                    </div>
-                    <div class="sb-sidenav-menu-heading">Addons</div>
+                        <div class="sb-nav-link-icon">
+                            <i class="fas fa-columns"></i>
+                        </div>
+                        Kelas
+                    </a>
+                    <a
+                        class="nav-link collapsed"
+                        href="#"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapsePages"
+                        aria-expanded="false"
+                        aria-controls="collapsePages"
+                    >
+                        <div class="sb-nav-link-icon">
+                            <i class="fas fa-user-md"></i>
+                        </div>
+                        Siswa
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Exam</div>
                     <a class="nav-link" href="charts.html">
                         <div class="sb-nav-link-icon">
                             <i class="fas fa-chart-area"></i>
                         </div>
-                        Charts
+                        Ujian
                     </a>
                     <a class="nav-link" href="tables.html">
                         <div class="sb-nav-link-icon">
                             <i class="fas fa-table"></i>
                         </div>
-                        Tables
+                        Ujian Session
                     </a>
                 </div>
             </div>
@@ -154,3 +78,14 @@
         </nav>
     </div>
 </template>
+
+<script>
+// import Link from "@inertiajs/inertia-vue3";
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        Link,
+    },
+};
+</script>
