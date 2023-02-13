234b1501c50130849fa91f1c6cf6cddcb427a3d4
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-13 11:59:34 +0900

    #

---
+++ resources/js/Pages/Admin/Exam/Create.vue
@@ -0,0 +1,132 @@
+<template>
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Tambah Ujian</h1>
+                <br />
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-6">
+                                <Link
+                                    href="/admin/lesson"
+                                    class="btn btn-primary"
+                                    ><i
+                                        class="fa fa-angle-left"
+                                        aria-hidden="true"
+                                    ></i>
+                                    Kembali</Link
+                                >
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5><i class="fa fa-bookmark"></i> Tambah Ujian</h5>
+                        <hr />
+                        <form @submit.prevent="submit">
+                            <div class="mb4">
+                                <label for="title">Nama Ujian</label>
+                                <input
+                                    type="text"
+                                    placeholder="Masukkan Nama Ujian"
+                                    class="form-control"
+                                    v-model="form.title"
+                                />
+                                <div class="invalid-feedback">
+                                    {{ errors.title }}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-4">
+                                        <label for="lesson">Kelas</label>
+                                        <select
+                                            v-model="form.classroom_id"
+                                            id="lesson"
+                                            class="form-select"
+                                        >
+                                            <option
+                                                v-for="classrooms in classroom"
+                                                :key="classrooms.id"
+                                                :value="classrooms.id"
+                                            >
+                                                {{ classrooms.title }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-4">
+                                        <label for="lesson"
+                                            >Mata Pelajaran</label
+                                        >
+                                        <select
+                                            v-model="form.lesson_id"
+                                            id="lesson"
+                                            class="form-select"
+                                        >
+                                            <option
+                                                v-for="lessons in lesson"
+                                                :key="lessons.id"
+                                                :value="lessons.id"
+                                            >
+                                                {{ lessons.title }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mb-4">
+                                <label for="description">Deskripsi</label>
+                                <Editor
+                                    api-key="tym8sv917r013htcr07qbxet8nmp7axloc18omawdb22nvay"
+                                    v-model="form.description"
+                                    :init="{
+                                        menubar: false,
+                                        plugins: 'lists link image emoticons',
+                                        toolbar:
+                                            'styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image emoticons',
+                                    }"
+                                />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import Layout from "../../../Layouts/Layout.vue";
+import Editor from "@tinymce/tinymce-vue";
+
+export default {
+    components: {
+        Editor,
+    },
+    layout: Layout,
+    props: {
+        errors: Object,
+        classroom: Object,
+        lesson: Object,
+    },
+    data() {
+        return {
+            form: {
+                title: "",
+                lesson_id: "",
+                classroom_id: "",
+                description: "",
+                duration: "",
+                random_question: "",
+                random_answer: "",
+                show_answer: "",
+            },
+        };
+    },
+    methods: {
+        submit() {},
+    },
+};
+</script>
---
+++ resources/js/Pages/Admin/Exam/Index.vue
@@ -0,0 +1,77 @@
+<template>
+    <Head>
+        <title>Exam</title>
+    </Head>
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Ujian</h1>
+                <ol class="breadcrumb mb-4"></ol>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-8">
+                                <Link
+                                    href="/admin/exam/create"
+                                    class="btn btn-md btn-primary"
+                                    ><i class="fas fa-plus"></i>
+                                    Tambah
+                                </Link>
+                            </div>
+                            <div class="col-4"></div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table id="datatablesSimple" class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Ujian</th>
+                                    <th>Pelajaran</th>
+                                    <th>Kelas</th>
+                                    <th>Jumlah Soal</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="exams in exam" :key="exams.id">
+                                    <td>1</td>
+                                    <td>{{ exams.title }}</td>
+                                    <td>{{ exams.lesson.title }}</td>
+                                    <td>{{ exams.classroom.title }}</td>
+                                    <td>{{ exams.question.length }}</td>
+                                    <td>
+                                        <Link
+                                            :href="`/admin/exam/show/${exams.id}`"
+                                            class="btn btn-default"
+                                            ><i class="fa fa-plus-circle"></i
+                                            >Show</Link
+                                        >
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import { Head } from "@inertiajs/inertia-vue3";
+import Layout from "../../../Layouts/Layout.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        Head,
+        Link,
+    },
+    layout: Layout,
+    props: {
+        exam: Object,
+    },
+};
+</script>
