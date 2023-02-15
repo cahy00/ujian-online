12bf9e3fd8bc62d8da623f0d2b8af44c2b0c7fc8
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-02-16 08:51:05 +0900

    #

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -43,7 +43,7 @@
                     </Link>
                     <Link
                         href="/admin/student"
-                        dssdsd
+                        dssdsdds
                         :class="{
                             active: $page.url.startsWith('/admin/student'),
                         }"
